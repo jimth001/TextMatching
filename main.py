c92388bc0a16180aecc4a709cc93d59468cb6d5c
jimth001 <rmwangyl@qq.com>
2018-02-05 19:19:02 +0800

    thulac分词时出现编码问题。新增sys.setdefaultencoding解决

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # coding:utf-8
 from BaseTensorFlow import RNNBasedModel
 from Preprocess.Preprocessor import Preprocessor
+import sys
 
 if __name__ == "main":
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
     config = RNNBasedModel.RNNConfig("my_rnn")
     rnn = RNNBasedModel.RNNModel(config)
     rnn.train("./training_testing/nlpcc-iccpol-2016.dbqa.training-data", save_and_quit=True, weight_balanced=True)
